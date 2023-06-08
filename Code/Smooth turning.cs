790b63dc4280e5a17e825e648d70db92b795202e
SpacerYes <83926400+SpacerYes@users.noreply.github.com>
2023-06-08 10:55:10 -0500

    Update Smooth turning.cs

--- Code/Smooth turning.cs
+++ Code/Smooth turning.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
+using UnityEngine.XR;
 
-public class OculusTurningScript : MonoBehaviour
+public class SmoothTurn : MonoBehaviour
 {
-	public float turningSpeed = 1.0f;
-	public string thumbstickHorizontalAxisName = "OculusThumbstickHorizontal";
-	void Update()
-	{
-		float turnInput = Input.GetAxis(thumbstickHorizontalAxisName);
+    public float turningSpeed = 1.0f;
 
-		//Your favorite turning logic >:)
+    void Update()
+    {
+        float turnInput = Input.GetAxis("OculusThumbstickHorizontal");
+
+        // Apply turning logic here
         Vector3 rotation = transform.rotation.eulerAngles;
         rotation.y += turnInput * turningSpeed * Time.deltaTime;
         transform.rotation = Quaternion.Euler(rotation);
-	}
+    }
 }
